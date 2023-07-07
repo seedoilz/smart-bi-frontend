addd9a8b122f61e542957b8a8b0df5a3d398a90b
seedoilz <seedoilz609@qq.com>
2023-07-07 18:41:06 +0800

    分离

--- src/app.tsx
+++ src/app.tsx
@@ -120,7 +120,7 @@ export const layout: RunTimeLayoutConfig = ({ initialState, setInitialState }) =
  * @doc https://umijs.org/docs/max/request#配置
  */
 export const request = {
-  baseURL: 'http://118.89.203.102:8101',
+  baseURL: 'http://localhost:8101',
   withCredentials: true,
   ...errorConfig,
 };
