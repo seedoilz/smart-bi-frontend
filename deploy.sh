ff9fe8967037a18c636a0bd3e6d898f00a93e961
seedoilz <seedoilz609@qq.com>
2023-07-07 14:18:08 +0800

    test deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,2 @@
 export PATH=$PATH:/node-v20.4.0-linux-x64/bin/
-rm -rf dist
-cnpm run build
-mv -f /var/lib/jenkins/workspace/smart_bi_frontend/dist /etc/nginx
-nginx -s reload
+nohup cnpm run start &
